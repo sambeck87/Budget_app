494ccd44474402b99765f27faa17785883c7bfbb
sambeck87 <sandro_mipb@hotmail.com>
2023-04-26 09:22:36 -0600

    fix linters

--- app/controllers/page_controller.rb
+++ app/controllers/page_controller.rb
@@ -1,8 +1,8 @@
 class PageController < ApplicationController
   skip_before_action :authenticate_user!, only: %i[index show]
   def index
-    if user_signed_in?
-      redirect_to user_groups_path(current_user.id)
-    end
+    return unless user_signed_in?
+
+    redirect_to user_groups_path(current_user.id)
   end
 end
